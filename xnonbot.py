fbda46480df29cd0404fa3a9649efdc9ad62d3ed
Quddus Salam <quddussalam555@gmail.com>
2023-05-18 22:02:23 +0700

    Migrated to discord-py-interactions Version 5x.

--- xnonbot.py
+++ xnonbot.py
@@ -1,17 +1,15 @@
-# XnonBot Version Beta 0.2.1
-# Currently written for interactions.py version 4 but it might be migrated to version 5 in the near future.
+# XnonBot Version Beta 0.2.2
+# Migrated to discord-py-interactions version 5
 import interactions
-import BotRequests
-import html
 import random
+import bot_req
+import html
 
-from interactions import Option, OptionType, CommandContext, ComponentContext, Button, ButtonStyle, ActionRow
-from dotenv import load_dotenv
 from os import getenv
+from dotenv import load_dotenv
 
 load_dotenv("C:\Programming\XnonBot\dev.env")
-token = getenv("XNONBOTTOKEN")  # Token for XnonBot#8699 on discord
-bot = interactions.Client(token=token)
+bot = interactions.Client()
 
 correct_trivia_answer = None
 
@@ -36,89 +34,80 @@ List of avalaible commands (prompted using </> command):
 """
 
 
-@bot.event
-async def on_ready():
-    print("The bot is now running!")
+@interactions.listen()
+async def on_startup():
+    print(f"The bot is running as {bot.user}!")
+    print()
 
 
-@bot.command(name="hello", description="Say hello to the user.")
-async def hello(ctx: CommandContext):
+@interactions.slash_command(name="hello", description="Say hello to the user.")
+async def hello(ctx: interactions.SlashContext):
     await ctx.send(f"Hello there {ctx.user.mention}!")
 
 
-@bot.command(name="help", description="Send a list of available slash commands.")
-async def help(ctx: CommandContext):
+@interactions.slash_command(name="help", description="Send a list of available slash commands.")
+async def help(ctx: interactions.SlashContext):
     await ctx.send(COMMANDS)
 
 
-@bot.command(
-    name="say",
-    description="Tell the bot to say something.",
-    options=[
-        Option(
-            type=OptionType.STRING,
-            name="message",
-            description="Message to send",
-            required=True,
-        ),
-    ],
+@interactions.slash_command(name="say", description="Tell the bot to say something.")
+@interactions.slash_option(
+    opt_type=interactions.OptionType.STRING,
+    name="message",
+    description="Message to send",
+    required=True
 )
-async def say(ctx: CommandContext, message: str):
+async def say(ctx: interactions.SlashContext, message: str):
     await ctx.send(f"{ctx.user.mention} said: `{message}`")
 
 
-@bot.command(name="about", description="Send the information about this bot.")
-async def about(ctx: CommandContext):
+@interactions.slash_command(name="about", description="Send the information about this bot.")
+async def about(ctx: interactions.SlashContext):
     await ctx.send("As the name implies, I'm a simple chatbot created by XnonXte!")
 
 
-@bot.command(name="roll", description="Choose a random dice roll (1 to 6).")
-async def roll(ctx: CommandContext):
+@interactions.slash_command(name="roll", description="Choose a random dice roll (1 to 6).")
+async def roll(ctx: interactions.SlashContext):
     await ctx.send(random.randint(1, 6))
 
 
-@bot.command(name="github", description="Send the github page for this bot.")
-async def github(ctx: CommandContext):
+@interactions.slash_command(name="github", description="Send the github page for this bot.")
+async def github(ctx: interactions.SlashContext):
     await ctx.send("https://github.com/XnonXte/XnonBot")
 
 
-@bot.command(name="quote", description="Get a random quote.")
-async def quote(ctx: CommandContext):
-    quote = BotRequests.get_quote()
+@interactions.slash_command(name="quote", description="Get a random quote.")
+async def quote(ctx: interactions.SlashContext):
+    quote = bot_req.get_quote()
     await ctx.send(quote)
 
 
-@bot.command(name="waifu", description="Get a random waifu picture.")
-async def waifu(ctx: CommandContext):
-    waifu = BotRequests.get_waifu_pic()
+@interactions.slash_command(name="waifu", description="Get a random waifu picture.")
+async def waifu(ctx: interactions.SlashContext):
+    waifu = bot_req.get_waifu_pic()
     await ctx.send(waifu)
 
 
-@bot.command(name="dog", description="Get a random dog picture.")
-async def dog(ctx: CommandContext):
-    dog = BotRequests.get_dog_pic()
+@interactions.slash_command(name="dog", description="Get a random dog picture.")
+async def dog(ctx: interactions.SlashContext):
+    dog = bot_req.get_dog_pic()
     await ctx.send(dog)
 
 
-@bot.command(name="cat", description="Get a random cat picture.")
-async def cat(ctx: CommandContext):
-    cat = BotRequests.get_cat_pic()
+@interactions.slash_command(name="cat", description="Get a random cat picture.")
+async def cat(ctx: interactions.SlashContext):
+    cat = bot_req.get_cat_pic()
     await ctx.send(cat)
 
 
-@bot.command(
-    name="rps",
-    description="Play rock, paper, scissors with the user.",
-    options=[
-        Option(
-            type=OptionType.STRING,
-            name="choice",
-            description="Input your choice (rock, paper, or scissors)",
-            required=True,
-        ),
-    ],
+@interactions.slash_command(name="rps", description="Play rock, paper, scissors with the user.")
+@interactions.slash_option(
+    opt_type=interactions.OptionType.STRING,
+    name="choice",
+    description="Enter your choice (rock, paper, scissors).",
+    required=True
 )
-async def rps(ctx: CommandContext, choice: str):
+async def rps(ctx: interactions.SlashContext, choice: str):
     choices = ("rock", "paper", "scissors")
     bot_choice = random.choice(choices)
 
@@ -144,141 +133,143 @@ async def rps(ctx: CommandContext, choice: str):
             f'{ctx.user.mention} chose {choice}. I chose {bot_choice}. I won!')
 
 
-@bot.command(
-    name="pexels",
-    description="Search an image on pexels.com",
-    options=[
-        Option(
-            type=OptionType.STRING,
-            name="search_query",
-            description="Image to search",
-            required=True
-        )
-    ]
+@interactions.slash_command(name="pexels", description="Search an image on pexels.com")
+@interactions.slash_option(
+    opt_type=interactions.OptionType.STRING,
+    name="search_query",
+    description="Image to search",
+    required=True
 )
-async def pexels(ctx: CommandContext, search_query: str):
-    image_output = BotRequests.get_pexels_photos(search_query)
+async def pexels(ctx: interactions.SlashContext, search_query: str):
+    image_output = bot_req.get_pexels_photos(search_query)
     await ctx.send(f"An image of {search_query} has been generated! Photographed by: {image_output[0]}, original link: {image_output[2]} - Powered by pexels.com")
 
 
-@bot.command(
-    name="animaltrivia",
-    description="Play a random animal trivia game.",
-)
-async def animaltrivia(ctx: CommandContext):
+@interactions.slash_command(name="animaltrivia", description="Play a random animal trivia game.")
+async def animaltrivia(ctx: interactions.SlashContext):
     global correct_trivia_answer
-    animal_trivia = BotRequests.get_animal_trivia()
+    animal_trivia = bot_req.get_animal_trivia()
     await ctx.send(html.unescape(f"{animal_trivia[0]} | The difficulty is {animal_trivia[1]}."))
 
-    button = Button(
-        style=ButtonStyle.PRIMARY,
-        custom_id="animal_trivia_button_true",
-        label="True",
-    )
-    button2 = Button(
-        style=ButtonStyle.DANGER,
-        custom_id="animal_trivia_button_false",
-        label="False",
-    )
-    action_row = ActionRow(components=[button, button2])
-    await ctx.send(f"Please choose an answer {ctx.user.mention}, on whether you think that is true or false.", components=action_row)
+    components: list[interactions.ActionRow] = [
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyle.PRIMARY,
+                custom_id="animal_trivia_button_true",
+                label="True",
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.DANGER,
+                custom_id="animal_trivia_button_false",
+                label="False"
+            )
+        )
+    ]
+
+    await ctx.send(f"Please choose an answer {ctx.user.mention}, on whether you think that is true or false.", components=components, ephemeral=True)
     correct_trivia_answer = animal_trivia[2]
 
 
-@bot.component("animal_trivia_button_true")
-async def animal_trivia_button_true(ctx: ComponentContext):
+@interactions.component_callback("animal_trivia_button_true")
+async def animal_trivia_button_true(ctx: interactions.ComponentContext):
     if correct_trivia_answer.lower() == "true":
         await ctx.send(f"{ctx.user.mention} choose True, {ctx.user.mention} is correct!")
     else:
         await ctx.send(f"{ctx.user.mention} choose True. Sorry, but the correct answer was {correct_trivia_answer}.")
 
 
-@bot.component("animal_trivia_button_false")
-async def animal_trivia_button_false(ctx: ComponentContext):
+@interactions.component_callback("animal_trivia_button_false")
+async def animal_trivia_button_false(ctx: interactions.ComponentContext):
     if correct_trivia_answer.lower() == "false":
         await ctx.send(f"{ctx.user.mention} choose False, {ctx.user.mention} is correct!")
     else:
         await ctx.send(f"{ctx.user.mention} choose False. Sorry, but the correct answer was {correct_trivia_answer}.")
 
 
-@bot.command(
+@interactions.slash_command(
     name="mathtrivia",
     description="Play a random math trivia game.",
 )
-async def mathtrivia(ctx: CommandContext):
+async def mathtrivia(ctx: interactions.SlashContext):
     global correct_trivia_answer
-    math_trivia = BotRequests.get_math_trivia()
+    math_trivia = bot_req.get_math_trivia()
     await ctx.send(html.unescape(f"{math_trivia[0]} | The difficulty is {math_trivia[1]}."))
 
-    button = Button(
-        style=ButtonStyle.PRIMARY,
-        custom_id="math_trivia_button_true",
-        label="True",
-    )
-    button2 = Button(
-        style=ButtonStyle.DANGER,
-        custom_id="math_trivia_button_false",
-        label="False",
-    )
-    action_row = ActionRow(components=[button, button2])
-    await ctx.send(f"Please choose an answer {ctx.user.mention}, on whether you think that is true or false.", components=action_row, ephemeral=True)
+    components = list[interactions.ActionRow] = [
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyle.PRIMARY,
+                custom_id="math_trivia_button_true",
+                label="True",
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.DANGER,
+                custom_id="math_trivia_button_false",
+                label="False"
+            )
+        )
+    ]
+    await ctx.send(f"Please choose an answer {ctx.user.mention}, on whether you think that is true or false.", components=components, ephemeral=True)
     correct_trivia_answer = math_trivia[2]
 
 
-@bot.component("math_trivia_button_true")
-async def math_trivia_button_true(ctx: ComponentContext):
+@interactions.component_callback("math_trivia_button_true")
+async def math_trivia_button_true(ctx: interactions.ComponentContext):
     if correct_trivia_answer.lower() == "true":
         await ctx.send(f"{ctx.user.mention} choose True, {ctx.user.mention} is correct!")
     else:
         await ctx.send(f"{ctx.user.mention} choose True. Sorry, but the correct answer was {correct_trivia_answer}")
 
 
-@bot.component("math_trivia_button_false")
-async def math_trivia_button_false(ctx: ComponentContext):
+@interactions.component_callback("math_trivia_button_false")
+async def math_trivia_button_false(ctx: interactions.ComponentContext):
     if correct_trivia_answer.lower() == "false":
         await ctx.send(f"{ctx.user.mention} choose False, {ctx.user.mention} is correct!")
     else:
         await ctx.send(f"{ctx.user.mention} choose False. Sorry, but the correct answer was {correct_trivia_answer}")
 
 
-@bot.command(
+@interactions.slash_command(
     name="animetrivia",
     description="Play a random animal trivia game.",
 )
-async def animetrivia(ctx: CommandContext):
+async def animetrivia(ctx: interactions.SlashContext):
     global correct_trivia_answer
-    anime_trivia = BotRequests.get_anime_trivia()
+    anime_trivia = bot_req.get_anime_trivia()
     await ctx.send(html.unescape(f"{anime_trivia[0]} | The difficulty is {anime_trivia[1]}."))
 
-    button = Button(
-        style=ButtonStyle.PRIMARY,
-        custom_id="anime_trivia_button_true",
-        label="True",
-    )
-    button2 = Button(
-        style=ButtonStyle.DANGER,
-        custom_id="anime_trivia_button_false",
-        label="False",
-    )
-    action_row = ActionRow(components=[button, button2])
-    await ctx.send(f"Please choose an answer {ctx.user.mention}, on whether you think that is true or false.", components=action_row, ephemeral=True)
+    components = list[interactions.ActionRow] = [
+        interactions.ActionRow(
+            interactions.Button(
+                style=interactions.ButtonStyle.PRIMARY,
+                custom_id="anime_trivia_button_true",
+                label="True",
+            ),
+            interactions.Button(
+                style=interactions.ButtonStyle.DANGER,
+                custom_id="anime_trivia_button_false",
+                label="False"
+            )
+        )
+    ]
+    await ctx.send(f"Please choose an answer {ctx.user.mention}, on whether you think that is true or false.", components=components, ephemeral=True)
     correct_trivia_answer = anime_trivia[2]
 
 
-@bot.component("anime_trivia_button_true")
-async def anime_trivia_button_true(ctx: ComponentContext):
+@interactions.component_callback("anime_trivia_button_true")
+async def anime_trivia_button_true(ctx: interactions.ComponentContext):
     if correct_trivia_answer.lower() == "true":
         await ctx.send(f"{ctx.user.mention} choose True, {ctx.user.mention} is correct!")
     else:
         await ctx.send(f"{ctx.user.mention} choose True. Sorry, but the correct answer was {correct_trivia_answer}")
 
 
-@bot.component("anime_trivia_button_false")
-async def anime_trivia_button_false(ctx: ComponentContext):
+@interactions.component_callback("anime_trivia_button_false")
+async def anime_trivia_button_false(ctx: interactions.ComponentContext):
     if correct_trivia_answer.lower() == "false":
         await ctx.send(f"{ctx.user.mention} choose False, {ctx.user.mention} is correct!")
     else:
         await ctx.send(f"{ctx.user.mention} choose False. Sorry, but the correct answer was {correct_trivia_answer}")
 
 
-bot.start()
+bot.start(getenv("XNONBOTTOKEN"))
